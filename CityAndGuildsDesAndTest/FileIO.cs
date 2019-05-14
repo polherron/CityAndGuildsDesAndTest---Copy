8e07b126fa5964bed82bbc1414912dbd14f61f8d
Paul Herron <paul.heron@nln.ie>
2019-05-14 10:24:08 +0100

    Added File IO

---
+++ CityAndGuildsDesAndTest/FileIO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityAndGuildsDesAndTest
+{
+    class FileIO
+    {
+        internal static void WriteToFile(string path, bool append, string courseRecord)
+        {
+                // Create a file to write to.
+                using (StreamWriter sw = new StreamWriter(path, append)) 
+                {
+                    sw.WriteLine(courseRecord);
+                }
+        }
+    }
+}
